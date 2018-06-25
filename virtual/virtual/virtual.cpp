b54c8eb613d94506c341a6e6a6177ba51c0b4c8c
20172105788 <39482047+20172105788@users.noreply.github.com>
2018-06-25 16:42:56 +0800

    Initial commit

---
+++ virtual/virtual/virtual.cpp
@@ -0,0 +1,35 @@
+// virtual.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+class CBase
+{
+public:
+	void display()
+	{
+		cout << "基类" << endl;
+	}
+};
+class CDervid :public CBase
+{
+public:
+	void test()
+	{
+
+	}
+	void display()
+	{
+		cout << "派生类" << endl;
+	}
+};
+
+int main()
+{
+	CBase obj1;
+	obj1.display();
+	CDervid obj2;
+	obj2.display();
+    return 0;
+}
